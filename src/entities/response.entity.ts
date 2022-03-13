7e9095b10bdd73bab02f28b9fe48bb40983519d5
tkgstrator <nasawake.am@gmail.com>
2022-03-13 16:40:08 +0900

    - add response entity

---
+++ src/entities/response.entity.ts
@@ -0,0 +1,28 @@
+import { Type } from 'class-transformer';
+
+export class ResponseEntity {
+  constructor() {
+    this.results = [];
+  }
+  @Type(() => ResultEntity)
+  results: ResultEntity[];
+}
+
+export class ResultEntity {
+  event_type: number;
+  water_level: number;
+  @Type(() => DistributionEntity)
+  distribution: DistributionEntity[];
+}
+
+export class DistributionEntity {
+  golden_ikura_num: number;
+  count: number;
+}
+
+export class SQLEntity {
+  golden_ikura_num: number;
+  event_type: number;
+  water_level: number;
+  count: number;
+}
